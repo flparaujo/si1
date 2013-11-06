ab12074975fbfc8b11580131e5b9b1ee26fafe52
Felipe Araujo de Andrade 21211043 <felipeaa@lcc2-22.lcc.ufcg.edu.br>
2013-11-06 10:36:35 -0300

    acrescentei um método

--- lab1/src/codigo/NumeroPorExtenso.java
+++ lab1/src/codigo/NumeroPorExtenso.java
@@ -2,7 +2,7 @@ package codigo;
 
 public class NumeroPorExtenso {
 	
-	private static String[] unidades = {"um", "dois", "três", "quatro", "cinco", "seis", "sete",
+	private static String[] unidades = {"zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete",
 		"oito", "nove"};
 	
 	private static String[] dezenas = {"dez", "onze", "doze", "treze", "catorze", "quinze", "dezesseis",
@@ -12,38 +12,54 @@ public class NumeroPorExtenso {
 	private static String[] centenas = {"cem", "duzentos", "trezentos", "quatrocentos", "quinhentos",
 		"seicentos", "setecentos", "oitocentos", "novecentos"};
 	
-	public static String unidade(int unidade) {
-		return unidades[unidade-1];
+	public static String unidadePorExtenso(int numero) {
+		return unidades[numero];
 	}
 	
-	public static String dezena(int dezena) {
-		if(dezena < 10) {
-			return unidades[dezena%10-1];
+	public static String dezenaPorExtenso(int numero) {
+		if(numero < 10) {
+			return unidades[numero%10];
 		}
-		if(dezena < 20 && dezena >= 10) {
-			return dezenas[dezena%10];
+		if(numero < 20 && numero >= 10) {
+			return dezenas[numero%10];
 		}
-		else if(dezena % 10 == 0) {
-			return dezenas[(dezena/10)+8];
+		else if(numero % 10 == 0) {
+			return dezenas[8+(numero/10)];
 		}
-		return dezenas[(dezena/10)+8] + " e " + unidade(dezena%10);
+		return dezenas[8+(numero/10)] + " e " + unidadePorExtenso(numero%10);
 	}
 	
-	public static String centena(int centena) {
-		if(centena/100 < 1) {
-			return dezena(centena%100);
+	public static String centenaPorExtenso(int numero) {
+		if(numero/100 < 1) {
+			return dezenaPorExtenso(numero%100);
 		}
-		else if(centena % 100 == 0) {
-			return centenas[centena/100-1];
+		else if(numero % 100 == 0) {
+			return centenas[numero/100-1];
 		}
-		else if(centena/100 == 1) {
-			return "cento e "+dezena(centena%100);
+		else if(numero/100 == 1) {
+			return "cento e "+dezenaPorExtenso(numero%100);
 		}
-		return centenas[centena/100-1] + " e " + dezena(centena%100);
+		return centenas[numero/100-1] + " e " + dezenaPorExtenso(numero%100);
 	}
 	
-	public static boolean ehUmNumeroNatural(String string) {
-		for(char caractere : string.toCharArray()) {
+	public static String milharPorExtenso(int numero) {
+		if(numero/1000 < 1) {
+			return centenaPorExtenso(numero);
+		}
+		else if(numero % 1000 == 0) {
+			return centenaPorExtenso(numero/1000).equals("um") ? "mil" : centenaPorExtenso(numero/1000) 
+					+ " mil";
+		}
+		else if(numero % 100 == 0 || ((numero % 1000)/100) == 0) {
+			return centenaPorExtenso(numero/1000).equals("um") ? "mil e " + centenaPorExtenso(numero%1000) : 
+				centenaPorExtenso(numero/1000) + " mil e " + centenaPorExtenso(numero%1000);
+		}
+		return centenaPorExtenso(numero/1000).equals("um") ? "mil " + centenaPorExtenso(numero%1000) : 
+			centenaPorExtenso(numero/1000) + " mil " + centenaPorExtenso(numero%1000);
+	}
+	
+	public static boolean ehUmNumeroNatural(String numero) {
+		for(char caractere : numero.toCharArray()) {
 			if(caractere < '0' || caractere > '9') {
 				return false;
 			}
@@ -59,7 +75,7 @@ public class NumeroPorExtenso {
 		if (numeroInteiro == 0) {
 			return "zero";
 		}
-		return centena(numeroInteiro);
+		return milharPorExtenso(numeroInteiro);
 	}
 
 }
