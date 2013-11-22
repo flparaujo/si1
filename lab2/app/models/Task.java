2ed7eee2ed043da1d34defccb313bb348384625b
Felipe Araujo de Andrade 21211043 <felipeaa@lcc2-23.lcc.ufcg.edu.br>
2013-11-22 13:28:18 -0300

    Tentando fazer a persistência

--- lab2/app/models/Task.java
+++ lab2/app/models/Task.java
@@ -2,22 +2,32 @@ package models;
 
 import java.util.*;
 
-import play.data.validation.Constraints.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
-public class Task {
-    
+import play.data.validation.Constraints.*;
+import play.db.ebean.Model;
+
+@Entity
+public class Task extends Model{
+   
+  @Id
   public Long id;
   @Required
   public String label;
+  
+  public static Finder<Long,Task> find = new Finder(Long.class, Task.class);
   
   public static List<Task> all() {
     return new ArrayList<Task>();
   }
   
   public static void create(Task task) {
+	  task.save();
   }
   
   public static void delete(Long id) {
+	  find.ref(id).delete();
   }
     
 }
