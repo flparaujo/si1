9981959d5f23b0b2900766acaf78ed0dd53a2ea3
Felipe Araújo <felipe.andrade@ccc.ufcg.edu.br>
2013-11-25 09:13:21 -0300

    modificado

--- lab2/conf/evolutions/default/1.sql
+++ lab2/conf/evolutions/default/1.sql
@@ -6,6 +6,8 @@
 create table task (
   id                        bigint not null,
   label                     varchar(255),
+  prioridade                integer,
+  projeto_associado         varchar(255),
   constraint pk_task primary key (id))
 ;
 
